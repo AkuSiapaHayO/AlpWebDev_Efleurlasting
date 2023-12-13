81706c4df1b34cdf760c665e7b0039968d1da9db
Karyna <karyna.sanjaya@gmail.com>
2023-12-13 17:51:07 +0700

    Product View

--- resources/views/Products/products.blade.php
+++ resources/views/Products/products.blade.php
@@ -11,38 +11,54 @@
                 @foreach ($categories as $category)
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">{{ $category->category_name }}</h5>
-                            <p class="card-text">{{ $category->description }}</p>
-                            <a href="{{ route('showcategory', ['category' => $category->id]) }}" class="btn btn-primary">View Category</a>
+                            <h5 class="card-title" style="height: 50px; overflow: hidden;">{{ $category->category_name }}</h5>
+                            <a href="{{ route('showcategory', ['category' => $category->id]) }}" class="btn btn-primary">View
+                                Category</a>
                         </div>
                     </div>
                 @endforeach
             </div>
         </div>
 
-        {{-- Display Products --}}
-        <div class="row">
-            @foreach ($products as $product)
-                <div class="col-md-4 mb-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h2 class="card-title">{{ $product->category->category_name }} - {{ $product->product_name }}</h2>
 
-                            {{-- Display the first associated image --}}
+        {{-- Display Products --}}
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-6">
+                @foreach ($products as $product)
+                    <div class="col mb-4">
+                        <div class="card position-relative">
+                            {{-- Check if the product has an image --}}
                             @if ($product->images->isNotEmpty())
-                                <div style="max-width: 100%; overflow: hidden;">
-                                    <img src="{{ asset('Assets/products/' . $product->images->first()->image_name) }}" alt="Product Image" class="img-fluid">
-                                </div>
+                                <img src="{{ asset('Assets/products/' . $product->images->first()->image_name) }}"
+                                    alt="Product Image" class="card-img-top img-fluid"
+                                    style="height: 250px; object-fit: cover;">
                             @else
-                                <p>No images available</p>
+                                {{-- Display a placeholder image or text when no image is available --}}
+                                <div class="card-img-top d-flex align-items-center justify-content-center"
+                                    style="height: 250px; background: rgba(255, 255, 255, 0.7);">
+                                    <p class="text-muted">No image available</p>
+                                </div>
                             @endif
+
+                            {{-- Text and link at the bottom of the card --}}
+                            <div class="card-body">
+                                <h6 class="card-title mb-0" style="max-height: 50px; overflow: hidden;">
+                                    {{ $product->category->category_name }} - {{ $product->product_name }}
+                                </h6>
+                                <a href="{{ route('showproduct', ['product' => $product->id]) }}"
+                                    class="btn btn-primary mt-2">View Details</a>
+                            </div>
                         </div>
-                        {{-- Add a link to view the product details --}}
-                        <a href="{{ route('showproduct', ['product' => $product->id]) }}" class="btn btn-primary">View Details</a>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
+
+
+
+
+
+
 
 
     </div>
